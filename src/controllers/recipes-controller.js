8254b956b19f85ef2b7b25e80a73126285c5e5ac
Vincent <vleclerc@inow.fr>
2019-06-18 22:13:04 +0200

    install mongoose

--- src/controllers/recipes-controller.js
+++ src/controllers/recipes-controller.js
@@ -1,11 +1,32 @@
+"use strict"
 
-
+// GET '/'
 function getAllRecipes(req, resp) {
     resp.json("");
 }
 
+// GET '/:id'
+exports.getRecipeById = (req, resp) => {
+    const id = req.params.id;
+    resp.json({ id });
+}
+
+// POST '/'
+exports.createRecipe = (req, resp) => {
+
+}
+
+// PUT '/:id'
+exports.updateRecipe = (req, resp) => {
+
+}
+
+// DELETE '/:id'
+exports.deleteRecipe = (req, resp) => {
+
+}
 
 exports.getAllRecipes = getAllRecipes;
-exports.toto = getAllRecipes;
+
 
 
--- src/routes/recipes-routes.js
+++ src/routes/recipes-routes.js
@@ -3,6 +3,15 @@ let express = require('express');
 
 let router = express.Router();
 
-router.route('/').get(controller.getAllRecipes);
+router.route('/')
+    .get(controller.getAllRecipes)
+    .post(controller.createRecipe);
+
+
+
+router.route('/:id')
+    .get(controller.getRecipeById)
+    .put(controller.updateRecipe)
+    .delete(controller.deleteRecipe);
 
 module.exports = router;
