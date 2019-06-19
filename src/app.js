f1a4ecc6e688956c87c5434d2f6adb460152bf44
Vincent <vleclerc@inow.fr>
2019-06-19 17:41:37 +0200

    fin du crud

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ let express = require('express');
 let mongoose = require('mongoose')
 
 //mid
+let bodyParser = require('body-parser');
 let recipesRoutesMid = require('./routes/recipes-routes');
 
 //conf
@@ -12,6 +13,9 @@ let app = express();
 
 mongoose.connect(conf.database);
 // mongoose.Promise = global.Promise; V4
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/api/recipes', recipesRoutesMid);
 
--- src/controllers/recipes-controller.js
+++ src/controllers/recipes-controller.js
@@ -7,11 +7,30 @@ function getAllRecipes(req, resp) {
         resp.json(data);
     });*/
 
+
+    test();
     recipe.find({}).then(
         (data) => {
             resp.json(data);
         }
     );
+}
+
+function traitement(resolve, reject) {
+    console.log('traitement');
+    const r = Math.random() * 100;
+    if (Math.floor(r) % 2 === 1) {
+        resolve(r);
+    } else {
+        reject({ message: `valeur de r: ${r}` });
+    }
+}
+
+function test() {
+    const prom = new Promise(traitement)
+        .then((num) => { console.log(`promise resolve: ${num}`) })
+        .catch((err) => { console.log(`promise reject: ${err.message}`) });
+
 }
 
 // GET '/:id'
@@ -22,17 +41,44 @@ exports.getRecipeById = (req, resp) => {
 
 // POST '/'
 exports.createRecipe = (req, resp) => {
-
+    console.log(req.body);
+    const obj = new recipe(req.body);
+    obj.save()
+        .then(
+            data => {
+                resp.status(201).json(data);
+            }
+        ).catch(err => {
+            console.log(err);
+            resp.status(400).json({ message: err.message || err })
+        });
 }
 
 // PUT '/:id'
 exports.updateRecipe = (req, resp) => {
-
+    const id = req.params.id;
+    const obj = req.body;
+    if (obj._id !== id) {
+        resp.status(400).end();
+        return;
+    } else {
+        recipe.findByIdAndUpdate(id, obj, { new: true })
+            .then(data => resp.json(data))
+            .catch(err => resp.status(400).json(err));
+    }
 }
 
 // DELETE '/:id'
 exports.deleteRecipe = (req, resp) => {
-
+    const id = req.params.id;
+    recipe.deleteOne({ _id: id })
+        .then((data) => {
+            if (data.deletedCount == 0)
+                resp.status(400).json({ message: `${data.deletedCount} élément supprimé.` })
+            else
+                resp.status(200).json({ message: `${data.deletedCount} élément supprimé.` })
+        })
+        .catch((err) => resp.status(400).json({ message: `Error: ${err}` }));
 }
 
 exports.getAllRecipes = getAllRecipes;
