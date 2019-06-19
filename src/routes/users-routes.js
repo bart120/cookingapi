54a8af92b5edbe34824d35956ff92c295afa8491
Vincent <vleclerc@inow.fr>
2019-06-19 22:23:05 +0200

    ajout users routes

---
+++ src/routes/users-routes.js
@@ -0,0 +1,17 @@
+let controller = require('../controllers/users-controller');
+let express = require('express');
+
+let router = express.Router();
+
+router.route('/')
+    .get(controller.getAllUsers)
+    .post(controller.createUser);
+
+
+
+router.route('/:id')
+    .get(controller.getUserById)
+    .put(controller.updateUser)
+    .delete(controller.deleteUser);
+
+module.exports = router;
